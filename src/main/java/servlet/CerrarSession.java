f3ef5fa8df07fc2fedced129d35ac110ae052822
ANGHELO <ANGHELO@DESKTOP-TKB56NU>
2023-12-01 22:47:33 -0500

    modificaciones

--- src/main/java/servlet/CerrarSession.java
+++ src/main/java/servlet/CerrarSession.java
@@ -6,7 +6,6 @@ package servlet;
 
 import Session.Sesion;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
